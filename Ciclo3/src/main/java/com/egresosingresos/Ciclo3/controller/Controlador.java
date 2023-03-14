27b0879b8ad02940e48fb30e7df1095d49d5a3db
DjeanG0 <jean.munoz@uptc.edu.co>
2023-03-14 12:43:56 -0500

    Enterprise CRUD with interface is completed

--- Ciclo3/src/main/java/com/egresosingresos/Ciclo3/controller/Controlador.java
+++ Ciclo3/src/main/java/com/egresosingresos/Ciclo3/controller/Controlador.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -39,6 +40,28 @@ public class Controlador {
         return "redirect:/AgregarEmpresa";
     }
 
-    
+    @GetMapping ("/EditarEmpresa/{id}")
+    public String editEmpresa(Model model, @PathVariable Integer id){
+        Empresa emp=empresaService.getEmpresaById(id);
+        model.addAttribute("emp",emp);
+        return "editarEmpresa";
+    }
 
+    @PostMapping ("/ActualizarEmpresa")
+    public String updateEmpresa(Empresa emp, RedirectAttributes redirectAttributes) {
+        if (empresaService.saveOrUpdateEmpresa(emp)){
+            return "redirect:/VerEmpresas";
+        }
+        return "redirect: /EditarEmpresa";
+    }
+
+    @GetMapping ("/EliminarEmpresa/{id}")
+    public String deleteEmpresa(@PathVariable Integer id){
+        try{
+            empresaService.deleteEmpresa(id);
+        }catch (Exception e){
+            return "redirect:/VerEmpresas";
+        }
+        return "redirect:/VerEmpresas";
+    }
 }
